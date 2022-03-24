3fe9ed07862ee149503704e235f404b8ad8c3783
Roberta Duarte <36114860+robertaduarte@users.noreply.github.com>
2022-03-24 12:39:32 -0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -8,14 +8,12 @@ import os
 
 model = create_auto_encoder(filters=args.filters)
 model = multi_gpu_model(model, gpus=2)
-path = "/DL/dl_coding/DL_code/Results/CNN64_20/"
+path = "/home/where_the_.h5_is"
 model.load_weights(path+"dl_fluids.h5")
 
-x_test = np.load("/DL/dl_coding/DL_code/Data/x.npy")
-#x_test = np.expand_dims(x_test[int(1892+236),:,:,:], axis=0)
+x_test = np.load("/home/data/")
 x_test = np.expand_dims(x_test[1784,:,:,:], axis=0)
 
-#r = 2366 - 1784
 for i in range(100):
 
        if i == 0:
