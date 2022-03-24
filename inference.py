5a08b54d05e6dd3af5efc07d25785eb23b083208
Roberta Duarte <36114860+robertaduarte@users.noreply.github.com>
2022-03-24 13:39:03 -0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,19 +1,28 @@
-from keras.models import Model, load_model
 import numpy as np
-from models import create_auto_encoder
-from params import args
+
+from keras.models import Model, load_model
 from keras.utils import multi_gpu_model
 
-import os
 
+### import the architecture and the arguments
+from models import create_auto_encoder
+from params import args
+
+
+#import the architecture 
+#and transform it to accept multi gpu 
 model = create_auto_encoder(filters=args.filters)
 model = multi_gpu_model(model, gpus=2)
+
+#the path where the .h5 is saved
 path = "/home/where_the_.h5_is"
 model.load_weights(path+"dl_fluids.h5")
 
+#load the input 
 x_test = np.load("/home/data/")
 x_test = np.expand_dims(x_test[1784,:,:,:], axis=0)
 
+#loop through the data
 for i in range(100):
 
        if i == 0:
